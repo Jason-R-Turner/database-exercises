ffcc27d6401c33288297a917d492c00c0f37d333
Jason-R-Turner <jason.r.turner22@gmail.com>
2022-04-04 22:18:34 -0500

    Exercises half finished

--- join_exercises.sql
+++ join_exercises.sql
@@ -19,11 +19,32 @@ SELECT users.name AS user_name, roles.name AS role_name
 FROM users
 RIGHT JOIN roles ON users.role_id = roles.id;
 
+SELECT *
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id;
+
+SELECT roles.role_name
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id;
+
+SELECT roles.name
+FROM roles
+RIGHT JOIN users ON roles.id = roles_id;
+
+SELECT roles.name
+FROM roles
+RIGHT JOIN users ON roles.id = roles_id;
+
 #3 Although not explicitly covered in the lesson, aggregate functions like count can be used with join queries. Use count and the appropriate join type to get a list of roles along with the number of users that has the role. Hint: You will also need to use group by in the query.
 # users = left table, roles = right table
-SELECT users.name AS user_name, roles.name AS role_name
+SELECT count(users.name) AS user_name, roles.name AS role_name
 FROM users
-LEFT JOIN roles ON users.role_id = roles.id group by role_name having count(role_name);
+RIGHT JOIN roles ON users.role_id = roles.id group by roles.name;
+
+# changed roles.name to role_name which gave same result
+SELECT count(users.name) AS user_name, roles.name AS role_name
+FROM users
+RIGHT JOIN roles ON users.role_id = roles.id group by role_name;
 
 use employees;
 select * from employees;
@@ -43,6 +64,17 @@ JOIN departments AS d
   ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01';
 
+# Review; SELECT * FROM employees LIMIT 5;
+/*R SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS current_department_manager
+FROM employees AS e
+JOIN dept_manager AS dm
+  ON dm.emp_no = e.emp_no
+	AND to_date > CURDATE()
+JOIN departments AS d USING(dept_no)
+ORDER BY dept_name;*/
+# USING is almost the same thing as 'ON'. Using is faster, but ON is more flexible and will usually be cleaner
+
+
 #3 Find the name of all departments currently managed by women.
 SELECT d.dept_name AS Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Manager_Name
 FROM employees AS e
@@ -51,6 +83,8 @@ JOIN dept_manager AS dm
 JOIN departments AS d
   ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F';
+
+/* AND to_date > CURDATE() AND gender = 'F' JOIN departments AS d USING... */
 
 
 #4 Find the current titles of employees currently working in the Customer Service department.
@@ -63,8 +97,11 @@ JOIN titles AS t
 WHERE de.to_date = '9999-01-01' AND de.dept_no = 'd009';
 
 
+
+
 #5 Find the current salary of all current managers.
-SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS Name, s.salary
+-- Look at all tables that are relevant
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS Name, s.salary, dm.to_date, dm.emp_no
 FROM employees AS e
 JOIN dept_manager AS dm
   ON dm.emp_no = e.emp_no
@@ -72,29 +109,120 @@ JOIN departments AS d
   ON d.dept_no = dm.dept_no
 JOIN salaries AS s
 ON s.emp_no = dm.emp_no
-WHERE dm.to_date = '9999-01-01';
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01';
 
 #6 Find the number of current employees in each department.
-SELECT d.dept_no, d.dept_name, count(e.emp_no) AS num_employees
+SELECT 
+    d.dept_no, d.dept_name, COUNT(dept_name) AS num_employees
+FROM
+    employees AS e
+        JOIN
+    dept_emp AS de ON de.emp_no = e.emp_no
+        JOIN
+    departments AS d ON d.dept_no = de.dept_no
+WHERE
+    de.to_date = '9999-01-01'
+GROUP BY dept_name
+ORDER BY dept_no;
+
+# if using only count it'd give you the sum of all employees.  If adding dept_no to the Select statement it'd add rows and subdivide the employees to match the rows
+
+#7 Which department has the highest average salary? Hint: Use current not historic information.
+describe dept_emp;
+
+SELECT d.dept_name, Round(avg(s.salary), 2) as average_salary
+FROM dept_emp AS de
+JOIN salaries AS s
+ON s.emp_no = de.emp_no
+	AND de.to_date > CURDATE()
+    AND s.to_date > CURDATE()
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+GROUP BY d.dept_name
+ORDER BY average_salary DESC Limit 1;
+
+/* SELECT d.dept_name, ROUND(AVG(salary), 2) AS average_salary FROM dept_emp AS de JOIN salaries AS s ON de.emp_no - s.emp_no AND de.to_dat > CURDATE() AND s.to_date > CURDATE() JOIN departments as d ON de.dep_no = d.dept_ no Group BY...  */
+
+select avg(salary) as avg_salary from employees AS e
+JOIN salaries AS s
+  ON s.emp_no = e.emp_no;
+  
+select avg(salary) as avg_salary from employees AS e
+JOIN salaries AS s
+  ON s.emp_no = e.emp_no
+JOIN dept_emp AS de
+  ON de.emp_no = s.emp_no;
+  
+select avg(salary) as avg_salary from employees AS e
+JOIN salaries AS s
+  ON s.emp_no = e.emp_no
+JOIN dept_emp AS de
+  ON de.emp_no = s.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no;
+
+#8 Who is the highest paid employee in the Marketing department?
+SELECT first_name, last_name
 FROM employees AS e
 JOIN dept_emp AS de
   ON de.emp_no = e.emp_no
 JOIN departments AS d
   ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' Group by num_employees;
+JOIN salaries AS s
+  ON de.emp_no = s.emp_no
+WHERE de.dept_no = 'd001';
+
+SELECT first_name, last_name, s.salary
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+JOIN salaries AS s
+  ON de.emp_no = s.emp_no
+WHERE de.dept_no = 'd001';
+
+SELECT first_name, last_name, s.salary, d.dept_name
+FROM employees AS e
+JOIN dept_emp AS de
+  ON de.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = de.dept_no
+JOIN salaries AS s
+  ON de.emp_no = s.emp_no
+WHERE de.dept_no = 'd001' order by salary desc limit 1;
 
 
-#7 Which department has the highest average salary? Hint: Use current not historic information.
 
-
-#8 Who is the highest paid employee in the Marketing department?
-
+#R Their code is for current, but answer's the same
 
 #9 Which current department manager has the highest salary?
-
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS Name, s.salary, dm.to_date, dm.emp_no
+FROM employees AS e
+JOIN dept_manager AS dm
+  ON dm.emp_no = e.emp_no
+JOIN departments AS d
+  ON d.dept_no = dm.dept_no
+JOIN salaries AS s
+ON s.emp_no = dm.emp_no
+WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'Order By salary desc limit 1;
 
 #10 Determine the average salary for each department. Use all salary information and round your results.
+SELECT d.dept_name, ROUND(AVG(s.salary), 0) as average_salary
+FROM employees AS e
+JOIN dept_emp AS de ON de.emp_no = e.emp_no
+JOIN departments AS d ON d.dept_no = de.dept_no
+JOIN salaries AS s ON s.emp_no = de.emp_no
+GROUP BY d.dept_name
+ORDER BY average_salary;
 
+SELECT d.dept_name, ROUND(AVG(s.salary),0) AS avg_dept_salary
+FROM departments d
+JOIN dept_emp de ON d.dept_no = de.dept_no
+JOIN employees e ON e.emp_no = de.emp_no
+JOIN salaries s ON e.emp_no = s.emp_no
+GROUP BY d.dept_name
+ORDER BY avg_dept_salary DESC;
 
 #11 Bonus Find the names of all current employees, their department name, and their current manager's name.
 
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,1 +1,119 @@
-use employees
+use employees;
+
+SELECT emp_no, salary
+FROM salaries
+WHERE salary > (SELECT AVG(salary) FROM salaries WHERE to_date > CURDATE())
+AND to_date > CURDATE();
+
+SELECT emp_no, salary
+FROM salaries
+WHERE salary > 2 * (SELECT AVG(salary) FROM salaries WHERE to_date > CURDATE())
+AND to_date > CURDATE();
+# returns > 2x avg salary
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+)
+LIMIT 10;
+
+SELECT first_name, last_name, birth_date
+FROM employees
+WHERE emp_no = (
+    SELECT emp_no
+    FROM employees
+    WHERE emp_no = 101010
+);
+
+SELECT g.birth_date, g.emp_no, g.first_name from
+(
+    SELECT *
+    FROM employees
+    WHERE first_name like 'Geor%'
+) as g;
+
+SELECT g.first_name, g.last_name, salaries.salary
+FROM
+    (
+        SELECT *
+        FROM employees
+        WHERE first_name like 'Geor%'
+    ) as g
+JOIN salaries ON g.emp_no = salaries.emp_no
+WHERE to_date > CURDATE();
+
+describe salaries;
+
+#Q1 Find all the current employees with the same hire date as employee 101010 using a sub-query.
+describe dept_emp;
+select * from dept_emp limit 10;
+
+SELECT 
+    first_name, last_name, hire_date, to_date
+FROM
+    employees AS e
+        JOIN
+    dept_emp AS de ON de.emp_no = e.emp_no
+WHERE
+    hire_date = (SELECT 
+            hire_date
+        FROM
+            employees
+        WHERE
+            emp_no = 101010)
+        AND to_date > CURDATE();
+
+
+#Q2 Find all the titles ever held by all current employees with the first name Aamod.
+
+select first_name, last_name from employees where first_name = 'Aamod';
+describe titles;
+describe dept_emp;
+
+SELECT 
+    distinct first_name, title, de.to_date
+FROM
+    employees AS e
+        JOIN
+    titles AS t ON e.emp_no = t.emp_no
+        JOIN
+    dept_emp AS de ON de.emp_no = e.emp_no
+WHERE e.first_name = 'Aamod'
+        AND de.to_date > CURDATE();
+
+
+#Q3 How many people in the employees table are no longer working for the company? Give the answer in a comment in your code.
+describe employees;
+describe dept_emp;
+
+SELECT count(emp_no) as past_employees
+FROM (SELECT * FROM dept_emp WHERE to_date < CURDATE()) as x;
+
+#A3 total past employees = 91479
+
+
+#Q4 Find all the current department managers that are female. List their names in a comment in your code.
+
+#Q5 Find all the employees who currently have a higher salary than the companies overall, historical average salary.
+
+#Q6 How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
+
+-- Hint Number 1 You will likely use a combination of different kinds of subqueries.
+-- Hint Number 2 Consider that the following code will produce the z score for current salaries.
+
+-- Returns the historic z-scores for each salary
+-- Notice that there are 2 separate scalar subqueries involved
+SELECT salary,
+    (salary - (SELECT AVG(salary) FROM salaries))
+    /
+    (SELECT stddev(salary) FROM salaries) AS zscore
+FROM salaries;
+
+
+-- BONUS
+
+#B1 Find all the department names that currently have female managers.
+#B2 Find the first and last name of the employee with the highest salary.
+#B3 Find the department name that the employee with the highest salary works in.
