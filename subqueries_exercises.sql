363a459293df2e7aeae6037932bd60ae8866ffd1
Jason-R-Turner <jason.r.turner22@gmail.com>
2022-04-05 16:43:51 -0500

    Temp table exercises saved after being disconnected from local mysql host

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,54 +1,88 @@
 use employees;
 
-SELECT emp_no, salary
-FROM salaries
-WHERE salary > (SELECT AVG(salary) FROM salaries WHERE to_date > CURDATE())
-AND to_date > CURDATE();
+SELECT 
+    emp_no, salary
+FROM
+    salaries
+WHERE
+    salary > (SELECT 
+            AVG(salary)
+        FROM
+            salaries
+        WHERE
+            to_date > CURDATE())
+        AND to_date > CURDATE();
 
-SELECT emp_no, salary
-FROM salaries
-WHERE salary > 2 * (SELECT AVG(salary) FROM salaries WHERE to_date > CURDATE())
-AND to_date > CURDATE();
+SELECT 
+    emp_no, salary
+FROM
+    salaries
+WHERE
+    salary > 2 * (SELECT 
+            AVG(salary)
+        FROM
+            salaries
+        WHERE
+            to_date > CURDATE())
+        AND to_date > CURDATE();
 # returns > 2x avg salary
 
-SELECT first_name, last_name, birth_date
-FROM employees
-WHERE emp_no IN (
-    SELECT emp_no
-    FROM dept_manager
-)
+SELECT 
+    first_name, last_name, birth_date
+FROM
+    employees
+WHERE
+    emp_no IN (SELECT 
+            emp_no
+        FROM
+            dept_manager)
 LIMIT 10;
 
-SELECT first_name, last_name, birth_date
-FROM employees
-WHERE emp_no = (
-    SELECT emp_no
-    FROM employees
-    WHERE emp_no = 101010
-);
-
-SELECT g.birth_date, g.emp_no, g.first_name from
-(
-    SELECT *
-    FROM employees
-    WHERE first_name like 'Geor%'
-) as g;
-
-SELECT g.first_name, g.last_name, salaries.salary
+SELECT 
+    first_name, last_name, birth_date
 FROM
-    (
-        SELECT *
-        FROM employees
-        WHERE first_name like 'Geor%'
-    ) as g
-JOIN salaries ON g.emp_no = salaries.emp_no
-WHERE to_date > CURDATE();
+    employees
+WHERE
+    emp_no = (SELECT 
+            emp_no
+        FROM
+            employees
+        WHERE
+            emp_no = 101010);
+
+SELECT 
+    g.birth_date, g.emp_no, g.first_name
+FROM
+    (SELECT 
+        *
+    FROM
+        employees
+    WHERE
+        first_name LIKE 'Geor%') AS g;
+
+SELECT 
+    g.first_name, g.last_name, salaries.salary
+FROM
+    (SELECT 
+        *
+    FROM
+        employees
+    WHERE
+        first_name LIKE 'Geor%') AS g
+        JOIN
+    salaries ON g.emp_no = salaries.emp_no
+WHERE
+    to_date > CURDATE();
 
 describe salaries;
 
 #Q1 Find all the current employees with the same hire date as employee 101010 using a sub-query.
 describe dept_emp;
-select * from dept_emp limit 10;
+SELECT 
+    *
+FROM
+    dept_emp
+LIMIT 10;
 
 SELECT 
     first_name, last_name, hire_date, to_date
@@ -95,14 +129,150 @@ FROM (SELECT * FROM dept_emp WHERE to_date < CURDATE()) as x;
 
 
 #Q4 Find all the current department managers that are female. List their names in a comment in your code.
+SELECT 
+    d.dept_name AS Department_Name,
+    CONCAT(e.first_name, ' ', e.last_name) AS Manager_Name
+FROM
+    employees AS e
+        JOIN
+    dept_manager AS dm ON dm.emp_no = e.emp_no
+        JOIN
+    departments AS d ON d.dept_no = dm.dept_no
+WHERE
+    dm.to_date = '9999-01-01'
+        AND e.gender = 'F';
+#A4 The female department manager names are Isamu Legleitner, Kasten Sigstam, Leon DasSarma, and Hilary Kambil
 
 #Q5 Find all the employees who currently have a higher salary than the companies overall, historical average salary.
+SELECT 
+    emp_no, salary
+FROM
+    salaries
+WHERE
+    salary > 2 * (SELECT 
+            AVG(salary)
+        FROM
+            salaries)
+        AND to_date > CURDATE();
+
+#A5 returned 706 rows
 
 #Q6 How many current salaries are within 1 standard deviation of the current highest salary? (Hint: you can use a built in function to calculate the standard deviation.) What percentage of all salaries is this?
 
+SELECT 
+    MAX(salary), STDDEV(salary)
+FROM
+    salaries
+WHERE
+    to_date > CURDATE();
+
+SELECT 
+    MAX(salary)
+FROM
+    salaries
+WHERE
+    to_date > CURDATE();
+#158220
+
+SELECT 
+    STDDEV(salary)
+FROM
+    salaries
+WHERE
+    to_date > CURDATE();
+#17309.95933634675
+
+select count(*) from salaries;
+
+SELECT 
+    salary
+FROM
+    salaries
+WHERE
+    salary > ((SELECT 
+            MAX(salary)
+        FROM
+            salaries
+        WHERE
+            to_date > CURDATE()) - (SELECT 
+            STDDEV(salary)
+        FROM
+            salaries
+        WHERE
+            to_date > CURDATE()))
+        AND to_date > CURDATE();
+#A6.1 returned 83 rows
+
+select count(salary) from salaries;
+
+SELECT 
+    COUNT(salary) / (SELECT 
+            COUNT(salary)
+        FROM
+            salaries) * 100
+FROM
+    salaries
+WHERE
+    salary > ((SELECT 
+            MAX(salary)
+        FROM
+            salaries
+        WHERE
+            to_date > CURDATE()) - (SELECT 
+            STDDEV(salary)
+        FROM
+            salaries
+        WHERE
+            to_date > CURDATE()))
+        AND to_date > CURDATE();
+#A6.2 returned 0.0029%
+
+SELECT 
+    z.salary, z.zscore
+FROM
+    (SELECT 
+        salary,
+            (salary - (SELECT 
+                    AVG(salary)
+                FROM
+                    salaries
+                WHERE
+                    to_date > CURDATE())) / (SELECT 
+                    STDDEV(salary)
+                FROM
+                    salaries
+                WHERE
+                    to_date > CURDATE()) AS zscore
+    FROM
+        salaries WHERE
+    to_date > CURDATE()) AS z
+WHERE
+    zscore > ((SELECT 
+            MAX(z.zscore)
+        FROM
+            (SELECT 
+                salary,
+                    (salary - (SELECT 
+                            AVG(salary)
+                        FROM
+                            salaries
+                        WHERE
+                            to_date > CURDATE())) / (SELECT 
+                            STDDEV(salary)
+                        FROM
+                            salaries WHERE
+    to_date > CURDATE()) AS zscore
+            FROM
+                salaries
+            WHERE
+                to_date > CURDATE()) AS z) - 1);
+#A6.1b using zscore returned 83 rows including zscore
+
+
+
+
 -- Hint Number 1 You will likely use a combination of different kinds of subqueries.
 -- Hint Number 2 Consider that the following code will produce the z score for current salaries.
-
 -- Returns the historic z-scores for each salary
 -- Notice that there are 2 separate scalar subqueries involved
 SELECT salary,
@@ -110,6 +280,7 @@ SELECT salary,
     /
     (SELECT stddev(salary) FROM salaries) AS zscore
 FROM salaries;
+
 
 
 -- BONUS
